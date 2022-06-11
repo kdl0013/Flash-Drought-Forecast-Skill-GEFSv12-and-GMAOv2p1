84d106b7fbf6792d3ce5396b69f3ff80f82e9dc9
Kyle Lesinger <kdl0013@auburn.edu>
2022-06-11 08:19:26 -0500

    EDDI is working

--- 1c_EDDI.pyx
+++ 1c_EDDI.pyx
@@ -24,7 +24,6 @@ import gc
 dir1 = 'main_dir'
 start_ = int('start_init')
 end_ = start_ + int('init_step')
-model_NUM = int('model_number')
 model_NAM1 = 'model_name'
 
 # dir1 = '/home/kdl/Insync/OneDrive/NRT_CPC_Internship'
@@ -73,8 +72,11 @@ This code is re-factored from 1b_EDDI.py
 #%%    
 '''process SubX files and create EDDI values'''
 # def multiProcess_EDDI_SubX_TEST(_date):
-def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _date,str var):
+def EDDI_function(int start_,int end_,list init_date_list,str _date,str var):
     #_date=init_date_list[0]  
+    
+    
+    var_name='ETo' #Just used for creating dictionary from ETo files
     
     # #Make sure that the models aren't overwriting each other 
     # if model_num == 1:
@@ -95,10 +97,10 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
             
     init_date_list = return_date_list()    
 
-    print(f'Calculating {var} anomaly on SubX for {_date} and saving as .npy in {home_dir}/{var}_anomaly_mod{model_NUM}.') 
-    os.chdir(f'{home_dir}/EDDI_mod{model_NUM}')
+    print(f'Calculating EDDI anomaly on SubX for {_date} and saving as .nc4 in {home_dir}.') 
+    os.chdir(f'{home_dir}')
     cdef int week_lead
-    week_lead = 6
+    week_lead = 7
 
     #Used for eliminating iterating over grid cells that don't matter
     smerge_file = xr.open_dataset(f'{smerge_dir}/smerge_sm_merged_remap.nc4')
@@ -149,7 +151,7 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
     for i_Y in range(subx2[f'{variable}'].shape[3]):
         #for i_X in range(1):
          for i_X in range(subx2[f'{variable}'].shape[4]):
-            if _date == '1999-01-10' and model_NUM==0 and start_ ==0:
+            if _date == '1999-01-10':
                 print(f'Working on lat {i_Y} and lon {i_X}')
 
             if (np.count_nonzero(np.isnan(smerge_file.RZSM[0,i_Y,i_X].values)) !=1) or ((i_X == 38 and i_Y == 6) or (i_X ==38 and i_Y ==7)):
@@ -157,27 +159,33 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
             # if (np.count_nonzero(np.isnan(subx2.EDDI[0,0,10,i_Y,i_X].values)) !=1) or ((i_X == 38 and i_Y == 6) or (i_X ==38 and i_Y ==7)):
 
                 def dict1_subx2():
-                    cdef dict summation_ETo_modN
-                    cdef int idx,julian_d, end_julian, subtract, len_text, idx_lead
+                    cdef dict summation_ETo_mod0,summation_ETo_mod1,summation_ETo_mod2,summation_ETo_mod3
+                    cdef int idx, end_julian, subtract, len_text, idx_lead
                     cdef list date_out, b_julian_out, b_julian_out2
                     
-                    #append 7-day average from all files to a new dictionary, 
-                    summation_ETo_modN = {}
+                    #append 7-day average from all files to a new dictionary
+                    summation_ETo_mod0 = {}
+                    summation_ETo_mod1 = {}
+                    summation_ETo_mod2 = {}
+                    summation_ETo_mod3 = {}
                     
                     for idx,julian_d in enumerate(file_julian_list):
-                        # if i==1:
-                        #     break
-                        #You must julian_d + week_lead because with ETo you need 7-day looking backwards into the past. Must have 7 values.
-                        #Choose just model = 0 because we just need to know if there are 7-days total in any model
-                        
-                        
+                        #Only look at weekly leads
                         if idx % 7 == 0:
                             try:
-                                if (len(subx2[f'{variable}'].sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=model_NUM, X=i_X, Y=i_Y).values)) == 7:
-                                    summation_ETo_modN[f'{file_julian_list[idx+week_lead]}']=[]
-                                    summation_ETo_modN[f'{file_julian_list[idx+week_lead]}'].append({f'{_date}':np.nanmean(subx2[f'{variable}'].sel(lead=slice(julian_d,file_julian_list[idx+week_lead])).isel(S=0, model=model_NUM, X=i_X, Y=i_Y).values)})   
+                                summation_ETo_mod0[f'{julian_d}']=[]
+                                summation_ETo_mod0[f'{julian_d}'].append({f'{_date}':np.nanmean(subx2[f'{var_name}'].sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=0, X=i_X, Y=i_Y).values)})   
+                                
+                                summation_ETo_mod1[f'{julian_d}']=[]
+                                summation_ETo_mod1[f'{julian_d}'].append({f'{_date}':np.nanmean(subx2[f'{var_name}'].sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=1, X=i_X, Y=i_Y).values)})   
+                                
+                                summation_ETo_mod2[f'{julian_d}']=[]
+                                summation_ETo_mod2[f'{julian_d}'].append({f'{_date}':np.nanmean(subx2[f'{var_name}'].sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=2, X=i_X, Y=i_Y).values)})   
+                                
+                                summation_ETo_mod3[f'{julian_d}']=[]
+                                summation_ETo_mod3[f'{julian_d}'].append({f'{_date}':np.nanmean(subx2[f'{var_name}'].sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=3, X=i_X, Y=i_Y).values)})
                             except IndexError:
-                                pass
+                                pass                    
                     '''7-day mean of EDDI by index:
                     Next we will append to each julian day value in the key in the dictionary with
                     the same julian day from all files'''
@@ -229,16 +237,20 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
                                #Only look at idx up to 39 because we need a full 7 days of data in order to calculate EDDI
                                 if idx % 7 == 0:
                                     try:
-                                        summation_ETo_modN[f'{b_julian_out2[idx_lead]}'].append({f'{file[-14:-4]}':np.nanmean(Et_ref_open_f.ETo.sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=model_NUM, X=i_X, Y=i_Y).values)})   
+                                        summation_ETo_mod0[f'{val}'].append({f'{file[-14:-4]}':np.nanmean(Et_ref_open_f.ETo.sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=0, X=i_X, Y=i_Y).values)})   
+                                        summation_ETo_mod1[f'{val}'].append({f'{file[-14:-4]}':np.nanmean(Et_ref_open_f.ETo.sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=1, X=i_X, Y=i_Y).values)})   
+                                        summation_ETo_mod2[f'{val}'].append({f'{file[-14:-4]}':np.nanmean(Et_ref_open_f.ETo.sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=2, X=i_X, Y=i_Y).values)})   
+                                        summation_ETo_mod3[f'{val}'].append({f'{file[-14:-4]}':np.nanmean(Et_ref_open_f.ETo.sel(lead=slice(julian_d-7,julian_d)).isel(S=0, model=3, X=i_X, Y=i_Y).values)})   
                                    #Some shouldn't/can't be appended to dictionary because they are useless
                                     except KeyError:
                                         pass
                                     except IndexError:
                                         pass
-                    return(summation_ETo_modN)
+                                    
+                    return(summation_ETo_mod0,summation_ETo_mod1,summation_ETo_mod2,summation_ETo_mod3)
                 
                 #Contains the julian day value of the current file ETo_{_date} and the 7-day summation
-                ETo_7_day_modN= dict1_subx2()
+                ETo_7_day_mod0,ETo_7_day_mod1,ETo_7_day_mod2,ETo_7_day_mod3= dict1_subx2()
                                     
                 '''Now we have created a dictionary that contains the:
                     1.) index value is the julian day
@@ -302,7 +314,11 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
                         
                     return(out_eddi_dictionary)
                     
-                EDDI_dict_modN = compute_EDDI_on_ETo_7_day_sum_dict(ETo_7_day_modN)
+                EDDI_dict_mod0 = compute_EDDI_on_ETo_7_day_sum_dict(ETo_7_day_mod0)
+                EDDI_dict_mod1 = compute_EDDI_on_ETo_7_day_sum_dict(ETo_7_day_mod1)
+                EDDI_dict_mod2 = compute_EDDI_on_ETo_7_day_sum_dict(ETo_7_day_mod2)
+                EDDI_dict_mod3 = compute_EDDI_on_ETo_7_day_sum_dict(ETo_7_day_mod3)
+
 
                 '''Instead of re-looping through all of the files (very slow), we can start appending to 
                 files one by one with the data that we have already collected.
@@ -314,8 +330,6 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
                     cdef dict final_out_dictionary_all_eddi
                     cdef int idx,idxxx
                     cdef list sub_keys
-                    
-                    
                     
                     final_out_dictionary_all_eddi = {}
 
@@ -334,9 +348,10 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
                         
                     return(final_out_dictionary_all_eddi)
                 
-                EDDI_next_dict_modN = improve_EDDI_dictionary(ETo_7_day_modN, EDDI_dict_modN)
-
-
+                EDDI_next_dict_mod0 = improve_EDDI_dictionary(ETo_7_day_mod0, EDDI_dict_mod0)
+                EDDI_next_dict_mod1 = improve_EDDI_dictionary(ETo_7_day_mod1, EDDI_dict_mod1)
+                EDDI_next_dict_mod2 = improve_EDDI_dictionary(ETo_7_day_mod2, EDDI_dict_mod2)
+                EDDI_next_dict_mod3 = improve_EDDI_dictionary(ETo_7_day_mod3, EDDI_dict_mod3)
 
                 
                 '''Now that we have final_out_dictionary_all_eddi which contains the specific values for each init date for the currenly looped X,Y grid cell, 
@@ -346,17 +361,20 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
             
                 '''Now that we have created new files, we can append each file with the data that was found'''
                 
-                def add_to_npy_file(dict EDDI_next_dict_modN,  int model_NUM):
+                def add_to_nc_file(dict EDDI_next_dict_mod0,dict EDDI_next_dict_mod1,dict EDDI_next_dict_mod2,dict EDDI_next_dict_mod3):
                     cdef int idx_, index_val
                     cdef dict dic_init_and_eddi_val
                     cdef str i_val
                     
-                    for idx_,i_val in enumerate(EDDI_next_dict_modN):
+                    for idx_,i_val in enumerate(EDDI_next_dict_mod0):
                       
-                    #for some reason it's a list in a list, this fixes that, loop through julian day
-                        EDDI_final_dict = EDDI_next_dict_modN[f'{i_val}'][0]
+                        #for some reason it's a list in a list, this fixes that, loop through julian day
+                        EDDI_final_dict0 = EDDI_next_dict_mod0[f'{i_val}'][0]
+                        EDDI_final_dict1 = EDDI_next_dict_mod1[f'{i_val}'][0]
+                        EDDI_final_dict2 = EDDI_next_dict_mod2[f'{i_val}'][0]
+                        EDDI_final_dict3 = EDDI_next_dict_mod3[f'{i_val}'][0]
                         
-                        for dic_init_and_eddi_val in EDDI_final_dict:
+                        for idx,dic_init_and_eddi_val in enumerate(EDDI_final_dict0):
                             # print(dic_init_and_eddi_val)
                             #Open up the file and insert the value
                             init_day = list(dic_init_and_eddi_val.keys())[0]
@@ -365,56 +383,39 @@ def EDDI_function(int start_,int end_,int model_NUM,list init_date_list,str _dat
                             load before another file has finished filling in the file and causing
                             a ValueError: cannot reshape array of size 15328 into shape (2,4,45,27,59)'''
                             
-                            lead_values = np.load(f'EDDI_{init_day}_julian_lead.npy',allow_pickle=True)
+                            lead_values = np.load(f'{home_dir}/julian_lead_{init_day}.npy',allow_pickle=True)
                             
                             #add values by julian day
                             # fileOut = f'{var}_anomaly_{init_day}.npy'
                             fileOut = 'EDDI_{}.nc4'.format(init_day)
                             
-                            eddi_open = xr.open_dataset(fileOut)
+                            file_open = xr.open_dataset(fileOut)
+                            file_open.close()
+                            
                             index_val=np.where(lead_values == int(i_val))[0][0]
-                            eddi_open.Variables[0,index_val,i_Y,i_X] = list(dic_init_and_eddi_val.values())[0]
                             
-                            #For some reason it spits an error
+                            #Add data to netcdf file
+                            file_open.EDDI[0,0,index_val,i_Y,i_X] = list(EDDI_final_dict0[idx].values())[0]
+                            file_open.EDDI[0,1,index_val,i_Y,i_X] = list(EDDI_final_dict1[idx].values())[0]
+                            file_open.EDDI[0,2,index_val,i_Y,i_X] = list(EDDI_final_dict2[idx].values())[0]
+                            file_open.EDDI[0,3,index_val,i_Y,i_X] = list(EDDI_final_dict3[idx].values())[0]
+
    
-                            eddi_open.to_netcdf(path = fileOut, mode ='a', engine='scipy')
-                            eddi_open.close()
-                            # del eddi_open
-                            
-                        
-                            #OLD Way of doing it with .npy files
-                            # fileOut = f'{var}_anomaly_{init_day}.npy'
-                            
-                            # try:
-                            #     eddi_open = np.load(fileOut,allow_pickle=True)
-                            # except ValueError:
-                            #     os.system('sleep 2')
-                            #     eddi_open = np.load(fileOut,allow_pickle=True)
-                            
-                            # lead_values = np.load(f'{var}_anomaly_{init_day}_julian_lead.npy',allow_pickle=True)
-                            
-                            # #Sometimes we found some indexes that really don't matter 
-                            # try:
-                            #     index_val=np.where(lead_values == int(i_val))[0][0]
-                            #     eddi_open[0,index_val,i_Y,i_X] = list(dic_init_and_eddi_val.values())[0]
-                            #     np.save(fileOut,eddi_open)
-                            # except IndexError:
-                            #     np.save(fileOut,eddi_open)
-                            #     pass
+                            file_open.to_netcdf(path = fileOut, mode ='w', engine='scipy')
+                            file_open.close()
+                          
                     
-                add_to_npy_file(EDDI_next_dict_modN,model_NUM)
+                add_to_nc_file(EDDI_next_dict_mod0,EDDI_next_dict_mod1,EDDI_next_dict_mod2,EDDI_next_dict_mod3)
                                 
                 #release some memory (doesn't work as well as I'd hoped)
                 # del EDDI_next_dict_modN, EDDI_dict_modN, ETo_7_day_average_modN
                 
-    print(f'Completed date {_date} and saved into {home_dir}/EDDI_mod{model_NUM}.')
+    print(f'Completed date {_date} and saved into {home_dir}.')
     #save the dates that were completed to not re-run
-    if model_NUM == 3:
-        os.system(f'echo Completed {_date} >> {script_dir}/EDDI_completed_nc_{model_NAM1}.txt')
-    
-    #Testing to see if this can help in releasing memory
-    cdef str out = 'done'
-    return(out)
+
+    os.system(f'echo Completed {_date} >> {script_dir}/EDDI_completed_nc_{model_NAM1}.txt')
+
+    return()
 #%%
 '''For some odd reason, this function will keep allocating new memory (even though
 there is nothing visible in the function that I can remove). To get around this,
@@ -438,9 +439,9 @@ subset_completed_dates = [i[5:] for i in completed_dates]
 count=0
 for _date in init_date_list[start_:end_]:    
     if _date[5:] not in subset_completed_dates:
-        EDDI_function(start_, end_, model_NUM, init_date_list, _date,var)
+        EDDI_function(start_, end_, init_date_list, _date,var)
         count+=1
-        if count == 40:
+        if count == 25:
             exit()
 
 
